e4d39e4fd90a7d04e5a14f124800c627e3adb1c7
lussyanast <lussyanast@gmail.com>
2024-04-27 21:24:40 +0700

    Update navbar 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,39 +14,42 @@
     <body>
         <nav class="navbar navbar-light navbar-expand-lg bg-light">
             <div class="container flex justify-content-between">
-              <a class="navbar-link" href="#">
+              <a class="navbar-link" href="{{ route('home')}}">
                 <img class="h-32px" src="{{ url('assets/images/logo-green.png') }}" alt="CodeTalk Logo">
               </a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <ul class="navbar-nav mx-0 mx-lg-3">
+                  <li class="nav-item d-block d-lg-none d-xl-block">
+                    <a class="nav-link active" aria-current="page" href="{{ route('home')}}">Home</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                  </li>
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Dropdown
-                    </a>
-                    <ul class="dropdown-menu">
-                      <li><a class="dropdown-item" href="#">Action</a></li>
-                      <li><a class="dropdown-item" href="#">Another action</a></li>
-                      <li><hr class="dropdown-divider"></li>
-                      <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
+                    <a class="nav-link" href="#">Discussions</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
+                    <a class="nav-link" href="#">About Us</a>
+                  </li>
+              </ul>
+                <form class="d-flex w-100 me-4 my-2 my-lg-0" role="search" action="#" method="GET">
+                  <div class="input-group">
+                    <span class="input-group-text bg-white border-end-0">
+                      <img src="{{ url('assets/images/magnifier.png') }}" alt="Search">
+                    </span>
+                    <input class="form-control border-start-0 ps-0" type="search" 
+                      placeholder="Search" aria-label="Search" name="" value="">
+                  </div>
+                </form>
+                <ul>
+                  <li class="nav-item my-auto">
+                    <a class="nav-link text-nowrap" href="#">Log In</a>
+                  </li>
+                  <li class="nav-item ps-1 pe-0">
+                    <a class="btn btn-primary-green" href="#">Sign Up</a>
                   </li>
                 </ul>
-                <form class="d-flex" role="search">
-                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                  <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
               </div>
             </div>
           </nav>
