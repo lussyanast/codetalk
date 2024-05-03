c9b3d8c7c1b296b4a1ca4b10f14e49dbf9c168fa
lussyanast <lussyanast@gmail.com>
2024-05-03 11:53:00 +0700

    update body section 1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,48 +11,83 @@
 
         @vite(['resources/scss/app.scss', 'resources/js/app.js'])
     </head>
+
+    <header>
+      <nav class="navbar navbar-light navbar-expand-lg bg-light">
+        <div class="container flex justify-content-between">
+          <a class="navbar-link" href="{{ route('home')}}">
+            <img class="h-32px" src="{{ url('assets/images/logo-green.png') }}" alt="CodeTalk Logo">
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-0 mx-lg-3">
+              <li class="nav-item d-block d-lg-none d-xl-block">
+                <a class="nav-link active" aria-current="page" href="{{ route('home')}}">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Discussions</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-nowrap" href="#">About Us</a>
+              </li>
+          </ul>
+            <form class="d-flex w-100 me-4 my-2 my-lg-0" role="search" action="#" method="GET">
+              <div class="input-group">
+                <span class="input-group-text bg-white border-end-0">
+                  <img class="h-32px" src="{{ url('assets/images/magnifier.png') }}" alt="Search">
+                </span>
+                <input class="form-control border-start-0 ps-0" type="search" 
+                  placeholder="Search" aria-label="Search" name="" value="">
+              </div>
+            </form>
+            <ul class="navbar-nav ms-auto my-2 my-lg-0">
+              <li class="nav-item my-auto">
+                <a class="btn btn-primary-green" href="#">Log In</a>
+              </li>
+              <li class="nav-item ps-1 pe-0">
+                <a class="btn btn-primary-white" href="#">Sign Up</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+
     <body>
-        <nav class="navbar navbar-light navbar-expand-lg bg-light">
-            <div class="container flex justify-content-between">
-              <a class="navbar-link" href="{{ route('home')}}">
-                <img class="h-32px" src="{{ url('assets/images/logo-green.png') }}" alt="CodeTalk Logo">
-              </a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mx-0 mx-lg-3">
-                  <li class="nav-item d-block d-lg-none d-xl-block">
-                    <a class="nav-link active" aria-current="page" href="{{ route('home')}}">Home</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Discussions</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link text-nowrap" href="#">About Us</a>
-                  </li>
-              </ul>
-                <form class="d-flex w-100 me-4 my-2 my-lg-0" role="search" action="#" method="GET">
-                  <div class="input-group">
-                    <span class="input-group-text bg-white border-end-0">
-                      <img src="{{ url('assets/images/magnifier.png') }}" alt="Search">
-                    </span>
-                    <input class="form-control border-start-0 ps-0" type="search" 
-                      placeholder="Search" aria-label="Search" name="" value="">
-                  </div>
-                </form>
-                <ul class="navbar-nav ms-auto my-2 my-lg-0">
-                  <li class="nav-item my-auto">
-                    <a class="btn btn-primary-green border" href="#">Log In</a>
-                  </li>
-                  <li class="nav-item ps-1 pe-0">
-                    <a class="btn btn-primary-white border" href="#">Sign Up</a>
-                  </li>
-                </ul>
+          <section class="container hero bg-green margin-hero py-5">
+            <div class="row align-items-center h-100">
+              <div class="col-12 col-lg-6 center-text">
+                <h1>CodeTalk<br/></h1>
+                <h2>Community Forum</h2>
+                <p class="mb-4">Empowering the students to connect and learn</p>
+                <a href="#" class="btn btn-primary-white me-2 mb-2 mb-lg-0">Sign Up</a>
+                <a href="#" class="btn btn-primary-green mb-2 mb-lg-0">Join Discussions</a>
               </div>
             </div>
-          </nav>
+          </section>
+
+          <section class="container min-h-372px">
+            <div class="row">
+              <div class="col-12 col-lg-4 text-center">
+                <img class="promote-icon mb-2" src="{{ url('assets/images/discussions.png')}}" alt="Discussions">
+                <h2>Discussions</h2>
+                <p class="fs-3">34834</p>
+              </div>
+              <div class="col-12 col-lg-4 text-center">
+                <img class="promote-icon mb-2" src="{{ url('assets/images/answers.png')}}" alt="Answers">
+                <h2>Answers</h2>
+                <p class="fs-3">98666</p>
+              </div>
+              <div class="col-12 col-lg-4 text-center">
+                <img class="promote-icon mb-2" src="{{ url('assets/images/users.png')}}" alt="Users">
+                <h2>Users</h2>
+                <p class="fs-3">122</p>
+              </div>
+            </div>
+          </section>
 
           <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     </body>
